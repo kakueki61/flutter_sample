18d8194f4e676a05fc2edbdfa0aa3c393fbaec32
TakuyaKodama <t.kodama61@gmail.com>
2018-03-26 01:14:44 +0900

    Simple screen transition

---
+++ lib/next_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class NextScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Next screen'),
+      ),
+      body: new Center(
+        child: new FlatButton(
+          child: new Text('Back'),
+          textColor: Colors.white,
+          color: Colors.lightBlue,
+          onPressed: () => Navigator.pop(context)
+        )
+      )
+    );
+  }
+}
