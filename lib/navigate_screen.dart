5dd019f7f04b9d07155283e31fe3ca161cd38e78
TakuyaKodama <t.kodama61@gmail.com>
2018-04-14 02:08:54 +0900

    Passes String value to DetailScreen from NavigateScreen

--- lib/navigate_screen.dart
+++ lib/navigate_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'next_screen.dart';
+import 'detail_screen.dart';
 
 class NavigateScreen extends StatelessWidget {
   @override
@@ -9,21 +9,51 @@ class NavigateScreen extends StatelessWidget {
         title: new Text('Navigate screen'),
       ),
       body: new Center(
-        child: new RaisedButton(
-          child: new Text('Next'),
-          textColor: Colors.white,
-          color: Colors.lightBlue,
-          onPressed: () {
-            _onPressNext(context);
-          }
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: new RaisedButton(
+                child: new Text('A'),
+                textColor: Colors.white,
+                color: Colors.lightBlue,
+                onPressed: () {
+                  _navigateToNext(context, 'A');
+                }
+              )
+            ),
+            new Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: new RaisedButton(
+                child: new Text('B'),
+                textColor: Colors.white,
+                color: Colors.lightBlue,
+                onPressed: () {
+                  _navigateToNext(context, 'B');
+                }
+              )
+            ),
+            new Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: new RaisedButton(
+                child: new Text('C'),
+                textColor: Colors.white,
+                color: Colors.lightBlue,
+                onPressed: () {
+                  _navigateToNext(context, 'C');
+                }
+              )
+            )
+          ]
         )
       )
     );
   }
 
-  void _onPressNext(BuildContext context) {
+  void _navigateToNext(BuildContext context, String text) {
     Navigator.push(context, new MaterialPageRoute(builder: (context) =>
-      new NextScreen()
+      new DetailScreen(text: text)
     ));
   }
 }
